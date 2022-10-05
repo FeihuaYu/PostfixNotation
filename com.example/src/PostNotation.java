749a7c42abce8b70ba3c256797852885007f3a99
脸宝 <zbb@s-MacBook-Pro.local>
2022-10-05 21:20:06 +0100

    consider more edge cases and clean code

--- com.example/src/PostNotation.java
+++ com.example/src/PostNotation.java
@@ -1,8 +1,10 @@
 package src;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -13,6 +15,7 @@ import java.util.Stack;
 
 public class PostNotation{
     private Set<String> operatorsSet = new HashSet<>();
+    private String error = "#ERR";
 
     public PostNotation() {
         operatorsSet.add("+");
@@ -25,9 +28,10 @@ public class PostNotation{
     public List<List<String>> readFromCSV() throws IOException {
         List<List<String>> csvList = new LinkedList<>();
         List<String> list = new LinkedList<>();
+        String filePath = "postfix_notation​.csv";
 
         try {
-            try (BufferedReader br = new BufferedReader(new FileReader("postfix_notation​.csv"))) {
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
                 String line;
                 line = br.readLine();
 
@@ -51,56 +55,36 @@ public class PostNotation{
         List<String> list = new LinkedList<>();
         String[] strArray = operations.split("\\s+");
         for(String str : strArray) {
-            list.add(str);
+            if (str.trim().length() > 0) {
+                list.add(str);
+            } 
+                
+
+
         }
         return list;
     }
 
 
     public String calculatePostNotation(List<String> list, List<List<String>> csvList, int rowNum, int colNum) {
-        String error = "#ERR";
         Stack<String> stack = new Stack<>();
         double value = 0;
         boolean hasOperator = false;
 
-        if(list.isEmpty() || list.size()==0) {
-            return null;
-        }
-
-        if(list.size() == 1) {
-            if(operatorsSet.contains(list.get(0))){
-                System.out.println(error);
-                return error;
-            }
-        }
-
-
-        if(list.contains(error)){
+        // Check initial format of post notation
+        if(!checkPostNotation(list)) {
             return error;
         }
 
         for(int i=0;i<list.size();i++) {
             String str = list.get(i);
-
+            System.out.println("str is " + str);
             if(operatorsSet.contains(str)){
                 hasOperator = true;
-                if(!stack.isEmpty()) {
+                if(!stack.isEmpty() && stack.size() > 1) {
                     double a = Double.parseDouble(stack.pop());
                     double b = Double.parseDouble(stack.pop());
-                    switch(str) {
-                        case "+":
-                            value = b + a;
-                            break;
-                        case "-": 
-                            value = b - a;
-                            break;
-                        case "*": 
-                            value = b * a;
-                            break;    
-                        case "/": 
-                            value = b / a;
-                            break; 
-                    }
+                    value = operatePostNotation(str, a, b);
                     stack.add(String.valueOf(value));
                 }
             }else {
@@ -111,9 +95,11 @@ public class PostNotation{
                         System.out.println(error);
                         return error;
                     }
-
-                    str = csvList.get(index[0]).get(index[1]);
-
+                    if(csvList.size() > index[0] && csvList.get(i).size() > index[1]) {
+                        str = csvList.get(index[0]).get(index[1]);
+                    }else {
+                        str = error;
+                    }
                 }
                 stack.add(str);
             }
@@ -121,6 +107,11 @@ public class PostNotation{
         }
 
         if(!hasOperator && list.size()>1) {
+            System.out.println(error);
+            return error;
+        }
+
+        if(hasOperator && list.size()==2) {
             System.out.println(error);
             return error;
         }
@@ -163,13 +154,38 @@ public class PostNotation{
     }
 
 
+    //  Check initial format of post notation 
+    public boolean checkPostNotation(List<String> list) {
+        String regexNum = "[0-9]+"; 
+        String regexLetter = "[a-zA-Z]";
+        if(list.isEmpty() || list.size()==0) {
+            return false;
+        }
+
+        if(list.size() == 1) {
+            if(operatorsSet.contains(list.get(0))){
+                return false;
+            }
+            if(!list.get(0).matches(regexNum) && !containsLetterAndNum(list.get(0))) {
+                return false;
+            }
+            if(list.get(0).matches(regexLetter)) {
+                return false;
+            }
+        }
+
+        if(list.contains(error)){
+            return false;
+        }
+
+        return true;
+    }
+
     public void implementFunction() throws IOException {
         List<List<String>> csvList = new LinkedList<>();
-
         List<String> postNotationList = new LinkedList<>();
 
         csvList = readFromCSV();
-
 
         for(int i=0;i<csvList.size();i++) {
             for(int j=0;j<csvList.get(i).size();j++) {
@@ -179,5 +195,48 @@ public class PostNotation{
             }
         }
 
+        writeToCSV(csvList);
+
+    }
+
+
+
+    public double operatePostNotation(String str, double a, double b) {
+        double value = 0;
+        switch(str) {
+            case "+":
+                value = b + a;
+                break;
+            case "-": 
+                value = b - a;
+                break;
+            case "*": 
+                value = b * a;
+                break;    
+            case "/": 
+                value = b / a;
+                break; 
+        }
+
+        return value;
+    }
+
+
+    public void writeToCSV(List<List<String>> csvList) throws IOException {
+        File csvFile = new File("result.csv");
+        FileWriter fileWriter = new FileWriter(csvFile);
+
+        for(List<String> list : csvList) {
+            StringBuilder stringBuilder = new StringBuilder(list.size());
+            for(int i=0;i<list.size();i++) {
+                stringBuilder.append(list.get(i));
+                if(i != list.size()-1) {
+                    stringBuilder.append(",");
+                }
+            }
+            stringBuilder.append("\n");
+            fileWriter.write(stringBuilder.toString());
+        }   
+        fileWriter.close();
     }
 }
